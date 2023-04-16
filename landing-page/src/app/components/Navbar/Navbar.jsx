55325f32ef0fb269026eabbfc9b2c4246e4c6f0b
Clive tsungu <harmonytsungu@gmail.com>
2023-04-16 10:32:24 +0100

    layout

---
+++ landing-page/src/app/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+
+import { navLinks } from '@/app/constants';
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
---
+++ landing-page/src/app/constants/index.js
@@ -0,0 +1,13 @@
+//static texts to be shown on the application 
+
+export const navLinks = [
+    {
+        id: "home",
+        title: "Home",  
+    },
+    {
+        id: "about",
+        title: "About",
+    },
+
+]
